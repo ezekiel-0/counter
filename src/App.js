0376327ea96fc88796612b067fc1edc0a563e9c8
nameless <gomisezekielpaulantoine@gmail.com>
2024-04-28 09:32:09 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import Game from "./components/Game";
+import Details from "./components/details";
+ 
+export default function App() {
+  return (
+    <Details />,
+    <Game />
+  )
+}
---
+++ src/components/details.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Details(){
+
+    return (
+        <div>
+            <h1>Description</h1>
+            <p>Range les nombres par ordres croissant</p>
+        </div>
+    )
+}
+
